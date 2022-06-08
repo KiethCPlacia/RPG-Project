13418281679adcc4633499cb489b82fa06d45659
Kieth C. Placia <kcplacia@student.apc.edu.ph>
2022-06-08 09:59:42 +0800

    Combat
    Dependency, Attack Animation

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using RPG.Movement;
+using RPG.Core;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour 
+    public class Fighter : MonoBehaviour, IAction 
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update() 
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
             
             if (!GetIsInRange())
@@ -19,7 +24,17 @@ namespace RPG.Combat
             }
             else
             {
-                GetComponent<Mover>().Stop();
+                GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+            GetComponent<Animator>().SetTrigger("attack");
+            timeSinceLastAttack = 0;
             }
         }
 
@@ -30,6 +45,7 @@ namespace RPG.Combat
         
        public void Attack(CombatTarget combatTarget)
        {
+           GetComponent<ActionScheduler>().StartAction(this);
            target = combatTarget.transform;
        }
 
@@ -37,5 +53,11 @@ namespace RPG.Combat
        {
            target = null;
        } 
+
+       // Animation Event
+       void Hit() 
+       {
+
+       }
     }
 }
