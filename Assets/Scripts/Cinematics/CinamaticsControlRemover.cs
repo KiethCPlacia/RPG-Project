b736a512cac073eda5b446a7e21873a7d536f545
Kieth C. Placia <kcplacia@student.apc.edu.ph>
2022-06-26 19:02:51 +0800

    Cinematics

---
+++ Assets/Scripts/Cinematics/CinamaticsControlRemover.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+	public class CinamaticsControlRemover : MonoBehaviour
+	{
+		private void Start() 
+		{
+			
+		}
+		
+		void DisableControl()
+		{
+			print("DisableControl");
+		}
+
+		void EnableControl()
+		{
+			print("EnableControl");
+		}
+	}
+}
+
--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Movement;
